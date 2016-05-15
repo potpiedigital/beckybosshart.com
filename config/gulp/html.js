ffea4b1e0064ac499a0dbbb10edc909666519e43
Matt Hayes <matt@mysterycommand.com>
2016-05-15 01:14:42 -0700

    feat(gulp): add gulp tasks and related plugins

---
+++ config/gulp/html.js
@@ -0,0 +1,17 @@
+import gulp from 'gulp';
+import ghtmlmin from 'gulp-htmlmin';
+
+export default () => {
+    return gulp
+        .src(['source/*.html'])
+        .pipe(ghtmlmin({
+            removeComments: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            removeTagWhitespace: true,
+            removeAttributeQuotes: true,
+            removeRedundantAttributes: true,
+            quoteCharacter: '"',
+        }))
+        .pipe(gulp.dest('public'));
+}
